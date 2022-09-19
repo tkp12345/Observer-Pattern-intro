364227d6fe54771119dd96936759f5d44bafe199
roberto6385 <roberto@netand.co.kr>
2022-09-20 07:55:48 +0900

    feat : 옵저버 패턴 발표 자료 코드(최종)

--- ObservePattern.js
+++ ObservePattern.js
@@ -1,3 +1,7 @@
+class JSFile{
+    update(){}
+}
+
 class 이벤트저장소 {
     constructor() {
         this.JSFile_List = []
@@ -7,24 +11,41 @@ class 이벤트저장소 {
         this.JSFile_List.push(target)
     }
 
-    notify(content) {
-        this.JSFile_List.forEach(JSFile => JSFile.Like(content))
+    notify() {
+        console.log('등록된 JSFILE : ',this.JSFile_List)
+        this.JSFile_List.forEach(JSFile => JSFile.update())
     }
 }
 
 const store = new 이벤트저장소()
 
-const JSFile1 = { Like: content => console.log('JSFile1 : ' + content) }
-const JSFile2 = { Like: content => console.log('JSFile2 : ' + content) }
-const JSFile3 = { Like: content => console.log('JSFile3 : ' + content) }
+class JSFile1 extends JSFile {
+    update(){
+        console.log('JSFile1 좋아요 + 1')
+    }
 
-store.register(JSFile1)
-store.register(JSFile2)
-store.register(JSFile3)
+}
+class JSFile2 extends JSFile {
+    update(){
+        console.log('JSFile2 좋아요 + 1')
+    }
+}
+class JSFile3 extends JSFile {
+    update(){
+        console.log('JSFile3 좋아요 + 1')
+    }
+}
+
+const jsFile1 = new JSFile1();
+const jsFile2 = new JSFile2();
+const jsFile3 = new JSFile3();
+
+
+store.register(jsFile1)
+store.register(jsFile2)
+store.register(jsFile3)
 
 store.notify('좋아요 +1')
 // JSFile1 : '좋아요 +1'
 // JSFile2 : '좋아요 +1'
 // JSFile3 : '좋아요 +1'
-
-
