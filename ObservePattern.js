fcdf81a0861e38b8108cc6139b09e5ba63af8c8e
roberto6385 <roberto@netand.co.kr>
2022-09-20 08:40:30 +0900

    fix  : 옵저버 패턴 발표 자료 코드(최종)

--- ObservePattern.js
+++ ObservePattern.js
@@ -1,40 +1,43 @@
-class JSFile{
-    update(){}
-}
 
-class 이벤트저장소 {
-    constructor() {
-        this.JSFile_List = []
+    class JSFile{
+        update(){}
     }
 
-    register(target) {
-        this.JSFile_List.push(target)
+    class JSFile1 extends JSFile {
+        update(){
+            console.log('JSFile1 좋아요 + 1')
+        }
+
+    }
+    class JSFile2 extends JSFile {
+        update(){
+            console.log('JSFile2 좋아요 + 1')
+        }
+    }
+    class JSFile3 extends JSFile {
+        update(){
+            console.log('JSFile3 좋아요 + 1')
+        }
     }
 
-    notify() {
-        console.log('등록된 JSFILE : ',this.JSFile_List)
-        this.JSFile_List.forEach(JSFile => JSFile.update())
+
+
+    class 이벤트저장소 {
+        constructor() {
+            this.JSFile_List = []
+        }
+
+        register(target) {
+            this.JSFile_List.push(target)
+        }
+
+        notify() {
+            console.log('등록된 JSFILE : ',this.JSFile_List)
+            this.JSFile_List.forEach(JSFile => JSFile.update())
+        }
     }
-}
 
 const store = new 이벤트저장소()
-
-class JSFile1 extends JSFile {
-    update(){
-        console.log('JSFile1 좋아요 + 1')
-    }
-
-}
-class JSFile2 extends JSFile {
-    update(){
-        console.log('JSFile2 좋아요 + 1')
-    }
-}
-class JSFile3 extends JSFile {
-    update(){
-        console.log('JSFile3 좋아요 + 1')
-    }
-}
 
 const jsFile1 = new JSFile1();
 const jsFile2 = new JSFile2();
@@ -45,7 +48,9 @@ store.register(jsFile1)
 store.register(jsFile2)
 store.register(jsFile3)
 
-store.notify('좋아요 +1')
+store.notify()
+
+
 // JSFile1 : '좋아요 +1'
 // JSFile2 : '좋아요 +1'
 // JSFile3 : '좋아요 +1'
